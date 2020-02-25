e647676aa136b23f6be76bb636d371d437c21300
bvaughnsulit <bvaughnsulit@gmail.com>
2020-02-24 21:36:14 -0800

    added context menu options to change options

---
+++ background.js
@@ -0,0 +1,36 @@
+chrome.contextMenus.create({
+  id: "add-to-hidden-companies",
+  title: "Add '%s' to hidden companies",
+  contexts: ["selection"]
+})
+
+chrome.contextMenus.create({
+  id: "add-to-hidden-locations",
+  title: "Add '%s' to hidden locations",
+  contexts: ["selection"]
+})
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  chrome.storage.sync.get({
+    hiddenCompanies: '',
+    hiddenLocations: ''
+  }, (x) => {
+      if (info.menuItemId == "add-to-hidden-companies"){
+        const hiddenCompanies = x.hiddenCompanies
+        const company = info.selectionText.trim()
+        hiddenCompanies.push(company)
+        chrome.storage.sync.set({hiddenCompanies: hiddenCompanies}, (x) => {
+          alert(`Added ${company} to hidden companies`)
+        })
+      } else if (info.menuItemId == "add-to-hidden-locations") {
+        const hiddenLocations = x.hiddenLocations
+        const location = info.selectionText.trim()
+        hiddenLocations.push(location)
+        chrome.storage.sync.set({hiddenLocations: hiddenLocations}, (x) => {
+          alert(`Added ${location} to hidden location`)
+        })
+      } else {
+        alert("error saving options")
+      }
+    })
+  });
